d6956907a795485ea3a54d8ce2a2b0e82352bb73
jojomoomaw <joseph.moomaw@gmail.com>
2022-08-22 09:14:59 -0500

    categories page dynamically links to individual categories' pages, image does not appear from fragment. Individual pages load dynamically from catView

--- src/main/java/org/launchcode/adventureland/controllers/CategoryController.java
+++ src/main/java/org/launchcode/adventureland/controllers/CategoryController.java
@@ -1,10 +1,13 @@
 package org.launchcode.adventureland.controllers;
 
 import org.launchcode.adventureland.models.data.CategoryRepository;
+import org.launchcode.adventureland.models.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping(value = "categories")
@@ -13,11 +16,26 @@ public class CategoryController{
     @Autowired
     private CategoryRepository categoryRepository;
 
-    @GetMapping("")
-    public String category(Model model) {
+    @GetMapping
+    public String index(Model model) {
         model.addAttribute("title", "All Categories");
         model.addAttribute("categories", categoryRepository.findAll());
+
         return "categories/index";
+    }
+
+    // this will need items displayed below
+    @GetMapping("/{categoryId}")
+    public String displayViewCategory(Model model, @PathVariable  Integer categoryId) {
+
+        Optional optCategory = categoryRepository.findById(categoryId);
+        if (optCategory.isPresent()) {
+            Category category = (Category) optCategory.get();
+            model.addAttribute("category", category);
+            return "catView";
+        } else {
+            return "redirect:../";
+        }
     }
 
 }
--- src/main/java/org/launchcode/adventureland/controllers/HomeController.java
+++ src/main/java/org/launchcode/adventureland/controllers/HomeController.java
@@ -1,14 +1,15 @@
 package org.launchcode.adventureland.controllers;
 
-import org.launchcode.adventureland.models.data.CategoryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController  {
 
-    @Autowired
-    private CategoryRepository groupRepository;
+    @GetMapping
+    public String index() {
+        return "index";
+    }
 
 
 }
