ad34f3eb313ccb9a95f0d4c786575d5aaa154e10
jarohnj <jarohn.johnson@gmail.com>
2022-09-20 09:13:11 -0500

    styled search page added conditional header to about page and added about page to security config

--- src/main/java/org/launchcode/adventureland/controllers/AboutController.java
+++ src/main/java/org/launchcode/adventureland/controllers/AboutController.java
@@ -1,6 +1,12 @@
 package org.launchcode.adventureland.controllers;
 
+import org.launchcode.adventureland.models.User;
+import org.launchcode.adventureland.models.UserData;
+import org.launchcode.adventureland.models.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,9 +14,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class AboutController {
 
-    @GetMapping("about")
-    public String displayAbout(){
+    @Autowired
+    private UserRepository userRepository;
 
+    @GetMapping("about")
+    public String displayAbout(Model model){
+        if (UserData.isUserNotLoggedIn()) {
+            return "about/index";
+        }
+
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByEmail(email);
+        model.addAttribute("user", user);
         return "about/index";
 
     }
