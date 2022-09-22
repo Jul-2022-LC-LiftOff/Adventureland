2552a7bbea942cdcc34f8df0c622a2c3b641459e
jarohnj <jarohn.johnson@gmail.com>
2022-09-22 16:15:11 -0500

    added method to home controller for pill

--- src/main/java/org/launchcode/adventureland/controllers/HomeController.java
+++ src/main/java/org/launchcode/adventureland/controllers/HomeController.java
@@ -1,8 +1,11 @@
 package org.launchcode.adventureland.controllers;
 
+import org.launchcode.adventureland.models.CatData;
+import org.launchcode.adventureland.models.Equipment;
 import org.launchcode.adventureland.models.User;
 import org.launchcode.adventureland.models.UserData;
 import org.launchcode.adventureland.models.data.CategoryRepository;
+import org.launchcode.adventureland.models.data.EquipmentRepository;
 import org.launchcode.adventureland.models.data.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -11,12 +14,17 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController  {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private EquipmentRepository equipmentRepository;
 
     @GetMapping("")
     public String index(Model model) {
@@ -30,5 +38,22 @@ public class HomeController  {
         return "index";
     }
 
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchTerm){
+        Iterable<Equipment> equipment;
+
+        equipment = CatData.findByValue(searchTerm, equipmentRepository.findAll());
+
+        model.addAttribute("title", "Equipment containing: " + searchTerm);
+        model.addAttribute("equipments", equipment);
+
+        if (UserData.isUserNotLoggedIn()) {
+            return "searchResults";
+        }
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByEmail(email);
+        model.addAttribute("user", user);
+        return "searchResults";
+    }
 
 }
